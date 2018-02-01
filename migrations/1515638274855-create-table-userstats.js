02014f83055be99c5e13f502cdc6a2c774db4bc3
Mikael Nazarenko <eaglewarrior8837@gmail.com>
2018-02-02 02:51:10 +0400

    database migration done

--- migrations/1515638274855-create-table-userstats.js
+++ migrations/1515638274855-create-table-userstats.js
@@ -3,8 +3,11 @@ const table = 'userstats';
 const pg = require('peegee')(process.env.POSTRES_URL);
 
 const definition = {
-  // id: { type: 'serial', primaryKey: true },
-  // created: { type: 'integer', notNull: true },
+  id: { type: 'serial', primaryKey: true },
+  created: { type: 'date', notNull: true },
+  userId: { type: 'smallint', notNull: true },
+  jobs: { type: 'smallint', notNull: true },
+  records: { type: 'bigint', notNull: true },
 };
 
 exports.up = next => {
