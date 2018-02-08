cc049e8c8128d9f6752a32656222c3c96886def0
Mikael Nazarenko <eaglewarrior8837@gmail.com>
2018-02-09 00:26:49 +0400

    edit integration test added & tested

---
+++ test/integration/userstat/edit.js
@@ -0,0 +1,60 @@
+// dependencies
+const { test } = require('tap');
+const path = require('path');
+const merge = require('deepmerge');
+// const { apig, deepSort } = require('utils')();
+
+// constants
+const { BEARER_TOKEN } = process.env;
+const __ = module => path.resolve(__dirname, './../../../', module);
+const createEvent = (params, body) => ({
+  headers: {
+    Authorization: `Bearer ${BEARER_TOKEN}`,
+    'Content-Type': 'application/json',
+  },
+  pathParameters: params,
+  body: JSON.stringify(body),
+});
+
+// fixtures
+const { params, payload } = require(__('test/fixture/edit.js'));
+// 200 Valid request
+const eventOk = createEvent(params, payload);
+// 400 Invalid request body
+const eventInvalid = createEvent(params, merge(payload,
+  { group1: { source: { userstat: true } } }));
+// 401 Invalid bearer token value
+const eventUnauthorised = merge(createEvent(params, payload), {
+  headers: { Authorization: 'Bearer: InvalidTokenValue' },
+});
+
+// module
+const { handler } = require(__('lib/userstat/edit'));
+
+// tests
+test('valid request', t => {
+  handler(eventOk, {}, (err, { statusCode, body }) => {
+    const { msg } = JSON.parse(body);
+    t.strictSame(statusCode, 200);
+    t.strictSame(msg, 'OK');
+    t.end();
+  });
+});
+
+test('invalid request', t => {
+  handler(eventInvalid, {}, (err, { statusCode, body }) => {
+    const { msg } = JSON.parse(body);
+    t.strictSame(statusCode, 400);
+    t.strictSame(msg, 'Invalid Request');
+    t.end();
+  });
+});
+
+test('Unauthenticated request', t => {
+  handler(eventUnauthorised, {}, (err, { statusCode, body }) => {
+    const { msg } = JSON.parse(body);
+    t.strictSame(statusCode, 401);
+    t.strictSame(msg, 'Unauthorised');
+    t.end();
+  });
+});
